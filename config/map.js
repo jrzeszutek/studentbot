9af34ed369162574d5157dfcbc43932924a820dd
Adrian Łukasik <adilucas7@gmail.com>
2017-11-14 14:59:14 +0100

    Added URL map, Helper class and basic route actions for import and validation

---
+++ config/map.js
@@ -0,0 +1,8 @@
+module.exports = {
+	path: {
+		'ict/1': 'stacjonarne-teleinformatyka',
+		'ict/2': 'stacjonarne-teleinformatyka--2',
+		'it/1': 'stacjonarne-informatyka',
+		'it/2': 'stacjonarne-informatyka-wytwarzanie-oprogramowania'
+	}
+}
